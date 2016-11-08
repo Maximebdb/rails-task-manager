7256c7ce9e00e8c41f3e4954e80d66753abe8c2b
Maxime BDB <maxime@packyourskills.com>
2016-11-08 12:44:40 +0100

    playing with migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'index', to: 'tasks#index'
+
+  get 'show', to: 'tasks#show'
+
+  get 'new', to: 'tasks#new'
+
+  get 'create', to: 'tasks#create'
+
+  get 'edit', to: 'tasks#edit'
+
+  get 'update', to: 'tasks#update'
+
+  get 'destroy', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20161108110222_add_idto_tasks.rb
+++
@@ -1,4 +0,0 @@
-class AddIdtoTasks < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
